279424fa0b955325fc1ed8cd678f77a58aa8b7d9
Sean C Davis <scdavis41@gmail.com>
2016-08-01 09:07:14 -0400

    unread should default to true

--- db/migrate/20160801130053_create_notify_on_notifications.rb
+++ db/migrate/20160801130053_create_notify_on_notifications.rb
@@ -3,7 +3,7 @@ class CreateNotifyOnNotifications < ActiveRecord::Migration[5.0]
     create_table :notify_on_notifications do |t|
       t.integer :recipient_id
       t.integer :sender_id
-      t.boolean :unread
+      t.boolean :unread, :default => true
       t.integer :trigger_id
       t.string :trigger_type
 
