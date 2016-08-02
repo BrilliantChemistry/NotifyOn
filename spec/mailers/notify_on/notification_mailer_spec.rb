1c591755b04c4a96f7e27e332102f3350540456f
Sean C Davis <scdavis41@gmail.com>
2016-08-02 08:34:34 -0400

    fix class name so rspec can run

--- spec/mailers/notify_on/notification_mailer_spec.rb
+++ spec/mailers/notify_on/notification_mailer_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 module NotifyOn
-  RSpec.describe NotificationMailerMailer, type: :mailer do
+  RSpec.describe NotificationMailer, type: :mailer do
     pending "add some examples to (or delete) #{__FILE__}"
   end
 end
